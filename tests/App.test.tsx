b7d2da6d415abc7ed8d48385a88cad59af3d2e57
WanderFlame <113013199+WanderFlame@users.noreply.github.com>
2024-05-05 23:38:05 +0300

    add test environment dependency

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../src/App';
 
 test('App renders without crashing', () => {
